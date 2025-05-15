a2e360642e44f51ad50fc447ec2ba95c1e91d23c
Santiago Molina Orozco <santi020k@gmail.com>
2025-05-15 17:36:37 -0500

    Add vercel domine

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,7 @@
 import type { NextConfig } from 'next'
 
 const nextConfig: NextConfig = {
-  images: { domains: ['localhost', 'santi020k.me'] }
+  images: { domains: ['localhost', 'santi020k.me', 'vercel.app'] }
 }
 
 export default nextConfig
